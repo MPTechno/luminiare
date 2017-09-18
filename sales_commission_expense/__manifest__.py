12174cdd727de0808720243c533408310de3382c
Amit Patel <amitkumarnpatel@gmail.com>
2017-09-18 15:15:57 +0530

    [ADD] Add a commission module for the backend Journal Entries of the Commission

---
+++ sales_commission_expense/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Sales Order / Invoicing Commission",
+    "author": "HashMicro/Axcensa",
+    "version": "10.0.1.0",
+    "website": "www.hashmicro.com",
+    "category": "Sales/ Invoice Management",
+    "depends": ['l10n_sg','solum_global_commission','solum_invoice'],
+    "data": [
+        "data/account_data.xml",
+        "views/account_invoice_view.xml",
+    ],
+    'demo': [
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
